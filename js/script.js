bfd6755542bd0dbf8e741187fd66641f4443a8ef
Veroni4ka.leo@gmail.com <Veroni4ka.leo@gmail.com>
2021-05-04 19:13:32 +0300

    Lesson15Commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+const numberOfFilms = prompt('Сколько фильмов Вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -11,10 +11,29 @@ const personalMovieDB = {
 };
 
 for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов1?', '');
+    const a = prompt('Один из последних просмотренных фильмов1?', ''),
           b = prompt('На сколько оцените его?', '');
 
-    personalMovieDB.movies[a] = b;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+         personalMovieDB.movies[a] = b;
+         console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+
+   
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
 }
 
 console.log(personalMovieDB);
+
